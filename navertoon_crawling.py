b5d4e480c9be943089f00cc220a18c771ecaa1c9
mogandaz <mogandaz@gmail.com>
2015-07-26 00:25:33 +0900

    Making function for retrieve webtoon_id, title, author and ep_id

--- navertoon_crawling.py
+++ navertoon_crawling.py
@@ -31,3 +31,32 @@ def crawl_main_page():
         pageID += 1
 
     return titleIDs
+
+def crawl_toon_page(toon_id):
+
+    data_table = []
+    url = "http://comic.naver.com/bestChallenge/list.nhn?titleId=" + str(toon_id)
+    response = requests.get(url)
+    html_string = response.text
+    dom = lxml.html.fromstring(html_string)
+
+    author = dom.cssselect("div[class='detail'] h2 span")[0].text_content()
+    title = dom.cssselect("div[class='detail'] h2")[0].text_content()[:-len(author)]
+    ep_cnt = int(dom.cssselect("td[class='title'] a")[0].get('href')[44:])
+
+    for idx in range(2,ep_cnt):
+        record = {}
+
+        ep_url = "http://comic.naver.com/bestChallenge/detail.nhn?titleId=" + str(toon_id) + "&no=" + str(idx)
+        ep_response = requests.get(ep_url)
+        ep_html_string = ep_response.text
+        ep_dom = lxml.html.fromstring(ep_html_string)
+
+        record["webtoon_id"] = toon_id
+        record["title"] = title
+        record["author"] = author
+        record["ep_id"] = idx
+
+        data_table.append(record)
+
+    return data_table
